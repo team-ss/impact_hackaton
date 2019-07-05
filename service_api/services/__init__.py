2f220a8f559550b952e4fb9568abcda55b4d9784
ovsienkonikita <40335968+ovsienkonikita@users.noreply.github.com>
2019-07-05 09:25:09 +0300

    Tech/type hints (#3)
    * Type hints + mypy
    
    * idk how to resolve missing import in numpy, it's unbelievable

--- service_api/services/__init__.py
+++ service_api/services/__init__.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
+from typing import Optional
 from urllib.parse import urlencode
 
 import aiohttp
 import aioredis
 import ujson
+from aioredis.commands import Redis
 from sanic.log import logger
 
 from service_api.domain.decorators import asyncio_task
@@ -22,10 +24,10 @@ class ResponseWrapper:
 
 
 class RedisCacheManager:
-    conn = None
+    conn: Redis = None
 
     @classmethod
-    async def get_conn(cls, redis_url):
+    async def get_conn(cls, redis_url: str) -> None:
         if not cls.conn:
             cls.conn = await aioredis.create_redis(redis_url)
 
@@ -36,24 +38,26 @@ class RedisCacheManager:
             await cls.conn.wait_closed()
 
     @classmethod
-    async def check_cache(cls, url, headers):
-        return await cls.conn.get(cls.__create_key(url, **headers))
+    async def check_cache(cls, url: str, headers: Optional[dict] = None) -> Optional[bytes]:
+        return await cls.conn.get(cls.__create_key(url, headers))
 
     @classmethod
     @asyncio_task
-    async def cache_data(cls, url, request_headers, response, ttl=180):
-        key = cls.__create_key(url, **request_headers)
+    async def cache_data(cls, url: str, response: ResponseWrapper, request_headers: Optional[dict],
+                         ttl: int = 180) -> int:
+        key = cls.__create_key(url, request_headers)
         hash_data = {
             'status': response.status,
             'headers': response.headers,
             'data': response.data
         }
-        json = ujson.encode(hash_data, ensure_ascii=False)
+        json = ujson.dumps(hash_data, ensure_ascii=False)
         return await cls.conn.set(key, json, expire=ttl)
 
     @staticmethod
-    def __create_key(url, **headers):
-        data = dict(url=url, **headers)
+    def __create_key(url: str, headers: Optional[dict] = None) -> int:
+        data: dict = headers or {}
+        data['url'] = url
         return hash(frozenset(data))
 
 
@@ -64,39 +68,40 @@ class BaseRestClient:
     __cache_manager = RedisCacheManager
 
     @classmethod
-    async def get(cls, url, headers=None, **kwargs):
+    async def get(cls, url: str, headers: Optional[dict] = None, **kwargs) -> ResponseWrapper:
         params = urlencode(kwargs, True)
         request_url = f'{cls.api_url}/{url}?{params}'
         cache = await cls.__cache_manager.check_cache(request_url, headers)
         if cache:
-            cache_data = ujson.decode(cache, 'UTF-8')
+            cache_data = ujson.loads(cache)
             return ResponseWrapper(request_url=request_url, headers=cache_data['headers'], status=cache_data['status'],
                                    data=cache_data['data'])
         else:
             response = await cls.__make_http_request('GET', url, headers, params=params)
             if response.ok:
-                await cls.__cache_manager.cache_data(request_url, headers, response)
+                await cls.__cache_manager.cache_data(request_url, response, request_headers=headers)
             return response
 
     @classmethod
-    async def post(cls, url, headers=None, data=None):
+    async def post(cls, url: str, headers: Optional[dict] = None, data: Optional[dict] = None) -> ResponseWrapper:
         return await cls.__make_http_request('POST', url, headers, data=data)
 
     @classmethod
-    async def put(cls, url, headers=None, data=None):
+    async def put(cls, url: str, headers: Optional[dict] = None, data: Optional[dict] = None) -> ResponseWrapper:
         return await cls.__make_http_request('PUT', url, headers, data=data)
 
     @classmethod
-    async def patch(cls, url, headers=None, data=None):
+    async def patch(cls, url: str, headers: Optional[dict] = None, data: Optional[dict] = None) -> ResponseWrapper:
         return await cls.__make_http_request('PATCH', url, headers, data=data)
 
     @classmethod
-    async def delete(cls, url, headers=None, **kwargs):
+    async def delete(cls, url: str, headers: Optional[dict] = None, **kwargs) -> ResponseWrapper:
         params = urlencode(kwargs, True)
         return await cls.__make_http_request('DELETE', url, headers, params=params)
 
     @classmethod
-    async def __make_http_request(cls, method, url, headers, params=None, data=None):
+    async def __make_http_request(cls, method: str, url: str, headers: Optional[dict] = None,
+                                  params: Optional[str] = None, data: Optional[dict] = None) -> ResponseWrapper:
         request_url = f'{cls.api_url}/{url}?{params}'
         async with aiohttp.ClientSession() as session:
             logger.debug(f'Sending {method} request to {url}, headers: {headers}')
@@ -108,7 +113,7 @@ class BaseRestClient:
                 # type: ignore
                 except aiohttp.ContentTypeError:
                     return ResponseWrapper(request_url=request_url, headers=dict(response.headers),
-                                           status=response.status, data=response.content)
+                                           status=response.status, data=dict(error=response.text))
                 else:
                     return ResponseWrapper(request_url=request_url, headers=dict(response.headers),
                                            status=response.status,
