f7a8955748625d138ea7b94ec9929936db74cf13
ovsienkonikita <40335968+ovsienkonikita@users.noreply.github.com>
2019-07-08 14:25:37 +0300

    Update config.py
    typo

--- service_api/config.py
+++ service_api/config.py
@@ -7,7 +7,7 @@ class AppConfig:
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_NAME = os.getenv('DB_NAME')
-    DB_URI = os.getenv('DATABASE_URI', f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+    DB_URI = os.getenv('DATABASE_URL', f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
     REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', '')
     REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
     REDIS_PORT = os.getenv('REDIS_PORT', 6379)
